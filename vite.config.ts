0b476396e7d749056e6859be5832aa7d8cbc1b17
Maksym Vrutskyi <nabacap72@gmail.com>
2025-06-11 01:43:24 +0200

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     viteStaticCopy({
       targets: [
         {
-          src: 'src/assets',
-          dest: 'src'  // будет скопировано как dist/src/assets
+          src: 'src/assets/**/*',
+          dest: 'src/assets'
         }
       ]
     })
   ],
-  base: './', // важно для github pages
-});
+  base: './',
+})
